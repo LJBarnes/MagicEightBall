5c2c0d74c26bd9182786590751c703d1578b00c5
Leah Barnes <johnson.leahe@gmail.com>
2020-04-29 20:46:32 -0400

    initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var button = document.getElementById("btn");
+var input = document.getElementById("input");
+var eight = document.getElementById("eight");
+var text = document.getElementById("text");
+var options=["yes", "no", "maybe"];
+
+button.addEventListener("click", function() {
+    if (input.value <= 1) {
+        alert("Please type in your question!")
+    } else {
+        eight.innerText = "";
+        text.innerText="Answer";
+    }
+});
